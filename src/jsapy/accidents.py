1cfd1119228cc8958deb3a1d71456ed522b0b3e7
Erik_MS <desconocidoerik@gmail.com>
2025-01-02 17:59:26 +0100

    Improved errors detection in basic_rate

--- src/jsapy/accidents.py
+++ src/jsapy/accidents.py
@@ -6,7 +6,16 @@ def basic_rate(num, den, factor):
     num = np.atleast_1d(num)
     den = np.atleast_1d(den)
     
-    if not np.all(num > 0) or not np.all(den > 0):
+    #check data type
+    if not np.issubdtype(num.dtype, np.number):
+        raise TypeError("All elements in 'num' must be numeric.")
+    if not np.issubdtype(den.dtype, np.number):
+        raise TypeError("All elements in 'den' must be numeric.")
+    if not isinstance(factor, (int, float)):
+        raise TypeError("'factor' must be a numeric value.")
+    
+    #check values
+    if not np.all(num >= 0) or not np.all(den >= 0):
         raise ValueError("All values must be positive.")
     if factor <= 0:
         raise ValueError("Factor mus be positive numeric value.")
